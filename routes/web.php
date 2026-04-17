6f4d34340a29a01dac815e9d87447248caa99a6f
ernajelita <ernajelita@students.amikom.ac.id>
2026-04-17 10:39:12 +0700

    Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/tentang', function () {
     return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
@@ -26,4 +29,15 @@ Route::get('/bantuan', function () {
 // Route home untuk memudahkan navigasi
 Route::get('/', function () {
     return view('home');
+});
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/events', function () {
+    return view('admin.events');
+});
+
+Route::get('/transaction', function () {
+    return view('admin.transaction');
 });
